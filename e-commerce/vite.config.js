a9cda869ef537d22a6f8841f42cf59f7cd4ee5e3
Johnn <xhoni@crispybacon.it>
2021-09-28 09:42:51 +0200

    Open index.html by default

--- e-commerce/vite.config.js
+++ e-commerce/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    open: '/index.html',
+    host:true
+    // proxy:   try it
+  },
 })
